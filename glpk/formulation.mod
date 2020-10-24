fbba840c39fedba58a4273bf233a59ac59963dba
Henry <38134284+akahenry@users.noreply.github.com>
2020-10-24 15:44:08 -0300

    Add GLPK formulation and col to dat parser

---
+++ glpk/formulation.mod
@@ -0,0 +1,24 @@
+# Number of vertices
+param V integer >= 0;
+
+# Set of colors
+set COLORS := 0..(V-1);
+
+# Set of vertices
+set VERTICES := 0..(V-1);
+
+# Set of edges
+param E {i in VERTICES, j in VERTICES} binary default 0;
+
+# Represents if the color 'h' is used
+var y { h in COLORS } binary;
+
+# Represents if the color 'h' is used in vertex 'i'
+var x { i in VERTICES, h in COLORS } binary;
+
+# Minimize the number of used colors
+minimize colors: sum{h in COLORS} y[h];
+
+s.t. W1 {i in VERTICES} : sum{h in COLORS} x[i, h] = 1;
+
+s.t. W2 {i in VERTICES, j in VERTICES, h in COLORS} : E[i, j]*(x[i, h] + x[j, h]) <= E[i, j]*y[h];
---
+++ glpk/instances/parser.py
@@ -0,0 +1,47 @@
+import argparse
+
+parser = argparse.ArgumentParser(
+        description='.',
+        usage='parser.py [-h] [--input file.col] [--output file.dat]')
+
+parser.add_argument('-i', '--input', type=str, nargs=1,
+                    help='a col file containing the instance',
+                    required=True)
+
+parser.add_argument('-o', '--output', type=str, nargs=1,
+                    help='name of dat file to be created',
+                    required=True)
+
+args = parser.parse_args()
+
+if not str(args.input[0]).endswith('.col'):
+    print(f'Error: parameter --input must be a .col file ({args.input}).')
+    exit(-1)
+elif not str(args.output[0]).endswith('.dat'):
+    args.output = args.output + '.dat'
+
+input_filename = str(args.input[0])
+output_filename = str(args.output[0])
+
+with open(output_filename, 'w') as dat_file:
+    dat_file.write('data;\n')
+    with open(input_filename) as col_file:
+        for line in col_file:
+            if line.startswith('c '):
+                continue
+            elif line.startswith('p '):
+                parts = line.replace('\n', '').split(' ')
+                number_of_vertices = parts[2]
+                
+                dat_file.write(f'param V := {number_of_vertices};\n')
+                dat_file.write('param E := \n')
+            elif line.startswith('e '):
+                parts = line.replace('\n', '').split(' ')
+                first_vertex = parts[1]
+                second_vertex = parts[2]
+
+                dat_file.write(f'{first_vertex} {second_vertex} 1\n')
+    dat_file.write(';\n')
+    dat_file.write('end;')
+
+        
